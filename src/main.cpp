ebc257f9a6007d0ae6d24d376aadc95c99ce97f2
admin <admin.git@ishiyou.net>
2020-06-10 00:04:07 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,57 @@ void r10(){
     std::cout << "r10" << std::endl;
 }
 
+static volatile sig_atomic_t run = 1;
+static void sigterm (int sig) {
+    run = 0;
+}
 
+class ExampleDeliveryReportCb : public RdKafka::DeliveryReportCb {
+public:
+    void dr_cb (RdKafka::Message &message) {
+        /* If message.err() is non-zero the message delivery failed permanently
+         * for the message. */
+        if (message.err())
+            std::cerr << "% Message delivery failed: " << message.errstr() << std::endl;
+        else
+            std::cerr << "% Message delivered to topic " << message.topic_name() <<
+                      " [" << message.partition() << "] at offset " <<
+                      message.offset() << std::endl;
+    }
+};
 int main() {
+    std::string brokers = "abc";
+    std::string errstr;
+    RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+    RdKafka::Conf *tconf = RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC);
+    if (conf->set("bootstrap.servers", brokers, errstr) != RdKafka::Conf::CONF_OK) {
+        spdlog::error("Set kafka config bootstrap.servers error: {}", errstr);
+        return 1;
+    }
+    signal(SIGINT, sigterm);
+    signal(SIGTERM, sigterm);
+
+    ExampleDeliveryReportCb ex_dr_cb;
+
+    if (conf->set("dr_cb", &ex_dr_cb, errstr) != RdKafka::Conf::CONF_OK) {
+        std::cerr << errstr << std::endl;
+        exit(1);
+    }
+
+    
+    RdKafka::Producer *producer = RdKafka::Producer::create(conf, errstr);
+    if (!producer) {
+        spdlog::error("Failed to create kafka producer: {}", errstr);
+        return 1;
+    }
+    delete conf;
+    for (std::string line; run && std::getline(std::cin, line);) {
+        if (line.empty()) {
+            spdlog::error("line is empty", brokers);
+            producer->poll(0);
+            continue;
+        }
+    }
     const char *host = "192.168.1.1";
 
 //    const char *host = "127.0.0.1";
